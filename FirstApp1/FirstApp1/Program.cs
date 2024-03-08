7a3c7b8862df1a2d1a922a0a59d1a18953008bd0
s27662 <kacper@Iqp.pl>
2024-03-08 15:01:58 +0100

    3rd commit

--- FirstApp1/FirstApp1/Program.cs
+++ FirstApp1/FirstApp1/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
 int x=0;
-Console.ReadLine()
 Console.WriteLine("grrr pow");
+Console.WriteLine(x);
