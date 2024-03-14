b5040f157afc59689234db977aff70d78d3c35a5
Juks0 <129502580+Juks0@users.noreply.github.com>
2024-03-14 16:02:37 +0100

    Update Program.cs

--- FirstApp1/FirstApp1/Program.cs
+++ FirstApp1/FirstApp1/Program.cs
@@ -6,27 +6,15 @@ Console.WriteLine(x);
 
 static double GetAvrage(int[] numbers)
 {
-    double result = 0.0;
+    double result2 = 0.0;
     for (int i = 0; i < numbers.Length; i++)
     {
-        result += numbers[i];
+        result2 += numbers[i];
     }
 
-    return result / numbers.Length;
+    return result2 / numbers.Length;
 }
 
 int [] numbers = {1,2,3,4};
 double avrage = GetAvrage(numbers);
 Console.WriteLine(avrage);
-
-static double GetMax(int[] numbers)
-{
-    int number = 0;
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        if (number<i)
-        {
-            number = i;
-        }
-    }
-}
